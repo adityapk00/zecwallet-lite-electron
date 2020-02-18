50d40a5858d2af8417a74b334d30923a4f1d57b6
Aditya Kulkarni <adityapk@gmail.com>
2020-02-18 11:34:43 -0800

    Properly save wallet before exit

--- app/components/LoadingScreen.js
+++ app/components/LoadingScreen.js
@@ -1,20 +1,18 @@
 /* eslint-disable max-classes-per-file */
 import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router';
-import { remote, ipcRenderer } from 'electron';
+import { ipcRenderer } from 'electron';
 import native from '../../native/index.node';
 import routes from '../constants/routes.json';
 import { RPCConfig, Info } from './AppState';
 import RPC from '../rpc';
 import cstyles from './Common.css';
 import styles from './LoadingScreen.css';
-import { NO_CONNECTION } from '../utils/utils';
 import Logo from '../assets/img/logobig.gif';
 
 type Props = {
   setRPCConfig: (rpcConfig: RPCConfig) => void,
-  setInfo: (info: Info) => void,
-  history: PropTypes.object.isRequired
+  setInfo: (info: Info) => void
 };
 
 class LoadingScreenState {
@@ -31,9 +29,9 @@ class LoadingScreenState {
   constructor() {
     this.currentStatus = 'Loading...';
     this.loadingDone = false;
-    this.zcashdSpawned = 0;
-    this.getinfoRetryCount = 0;
     this.rpcConfig = null;
+    this.url = '';
+    this.getinfoRetryCount = 0;
   }
 }
 
@@ -54,20 +52,15 @@ class LoadingScreen extends Component<Props, LoadingScreenState> {
       const result = native.litelib_initialize_existing(false, url);
       console.log(`Intialization: ${result}`);
 
+      this.setupExitHandler();
       this.setupNextGetInfo();
     })();
   }
 
   setupExitHandler = () => {
-    // App is quitting, exit zcashd as well
+    // App is quitting, make sure to save the wallet properly.
     ipcRenderer.on('appquitting', () => {
-      if (this.zcashd) {
-        const { history } = this.props;
-
-        this.setState({ currentStatus: 'Waiting for zcashd to exit' });
-        history.push(routes.LOADING);
-        this.zcashd.kill();
-      }
+      RPC.doSave();
 
       // And reply that we're all done.
       ipcRenderer.send('appquitdone');
@@ -79,7 +72,7 @@ class LoadingScreen extends Component<Props, LoadingScreenState> {
   }
 
   async getInfo() {
-    const { url, zcashdSpawned, getinfoRetryCount } = this.state;
+    const { url } = this.state;
 
     // Try getting the info.
     try {
@@ -103,36 +96,9 @@ class LoadingScreen extends Component<Props, LoadingScreenState> {
         // This will cause a redirect to the dashboard
         this.setState({ loadingDone: true });
       })();
-
-      // Set up to save wallet before exit.
-      remote.getCurrentWindow().on('close', () => {
-        RPC.doSave();
-      });
     } catch (err) {
       // Not yet finished loading. So update the state, and setup the next refresh
       this.setState({ currentStatus: err });
-
-      if (err === NO_CONNECTION && !zcashdSpawned) {
-        // Try to start zcashd
-        this.startZcashd();
-        this.setupNextGetInfo();
-      }
-
-      if (err === NO_CONNECTION && zcashdSpawned && getinfoRetryCount < 10) {
-        this.setState({ currentStatus: 'Waiting for zcashd to start...' });
-        const inc = getinfoRetryCount + 1;
-        this.setState({ getinfoRetryCount: inc });
-        this.setupNextGetInfo();
-      }
-
-      if (err === NO_CONNECTION && zcashdSpawned && getinfoRetryCount >= 10) {
-        // Give up
-        this.setState({ currentStatus: 'Failed to start zcashd. Giving up!' });
-      }
-
-      if (err !== NO_CONNECTION) {
-        this.setupNextGetInfo();
-      }
     }
   }
 
